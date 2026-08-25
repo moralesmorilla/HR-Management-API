2c74492251e5e1dde7941460b8c1c64b00708d9e
moralesmorilla <moralesmorillamanuel@gmail.com>
2026-08-25 14:19:53 +0200

    feat: crear EmployeeRepository con Spring Data JPA

---
+++ src/main/java/com/manu/hr_management_api/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.manu.hr_management_api.repository;
+
+import com.manu.hr_management_api.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+
+}
